79b6bc83e428f459ce532de5f46651435deff5e2
Efarinyes <efarinyes@gmail.com>
2022-02-24 20:27:12 +0100

    Primer commit - Practiques Redux Reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import CounterReduxReducer from "./CounterReduxReducer";
+import { createStore } from "redux";
+import { counter, increment } from "./counter";
+
+const store = createStore(counter, 0);
+store.dispatch(increment(+1));
+console.log("state", store.getState());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CounterReduxReducer />
+      <CounterReduxReducer />
+    </>
   );
 }
-
 export default App;
---
+++ src/CounterReduxReducer.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useReducer } from "react";
+import { counter, increment, reset } from "./counter";
+
+function CounterReduxReducer() {
+  const [count, dispatch] = useReducer(counter, 0);
+
+  return (
+    <div className="App">
+      <h1> Comptador </h1>
+      <div> {count} </div>
+      <button onClick={() => dispatch(increment(+1))}>+1</button>
+      <button onClick={() => dispatch(reset())}>Resseteja</button>
+      <button onClick={() => dispatch(increment(-1))}>-1</button>
+    </div>
+  );
+}
+export default CounterReduxReducer;
