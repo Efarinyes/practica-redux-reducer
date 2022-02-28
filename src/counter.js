1c6436ffbf076f196826bfb5c11edfc007ae3ca1
Efarinyes <efarinyes@gmail.com>
2022-02-28 20:20:12 +0100

    Commit - Millores

--- src/counter.js
+++ src/counter.js
@@ -1,24 +1,22 @@
-const INCREMENT = "INCREMENT";
+const INCREMENT = "count/INCREMENT";
 export function increment(amount) {
   return { type: INCREMENT, amount };
 }
 
-const RESET = "RESET";
+const RESET = "count/RESET";
 export function reset() {
   return { type: RESET };
 }
 export function counter(state = 0, action) {
-  
   switch (action.type) {
-    case "INCREMENT":
+    case INCREMENT:
       return state + action.amount;
-    case "RESET":
+    case RESET:
       return 0;
     default:
       return state;
   }
 }
 export function getCount(state) {
-  console.log(state)
   return state.counter;
 }
